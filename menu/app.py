682ad67651d483810bb90983ce2c26cb354cbf73
ParkIlha <fafa0919999@gmail.com>
2022-11-30 00:19:56 +0900

    룰렛 추가

--- menu/app.py
+++ menu/app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__) 		
 
 @app.route("/") 		
-def hello():			
+def main():			
     return render_template("index.html")
+
+@app.route("/roulette") 		
+def roulette():			
+    return render_template("roulette.html")
 
 if __name__ == "__main__": 	
     app.run(debug=True)
