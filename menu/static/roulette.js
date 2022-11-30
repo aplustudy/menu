0e701ccbab0f8ccb8537e4004e1cf1aec08b39c6
ParkIlha <fafa0919999@gmail.com>
2022-11-30 23:34:53 +0900

    룰렛 기능 구현 완료

--- menu/static/roulette.js
+++ menu/static/roulette.js
@@ -1,35 +1,28 @@
 const $c = document.querySelector("canvas");
 const ctx = $c.getContext(`2d`);
 
-function foodGet(){
+function foodAll(){
   $.ajax({
       type: "GET",
       url: "/food_db",
       data: {},
       success: function (response) {
         const food = response['foods']
-        const korea = []
-        const china = []
-        const japen = []
-        const american = []
+        const allFood = []
         for(let i = 0; i < food.length; i++){
-          if(food[i].category === '한식'){
-            korea.push(food[i].name)
-          }
-          if(food[i].category === '중식'){
-            china.push(food[i].name)
-          }
-          if(food[i].category === '양식'){
-            american.push(food[i].name)
-          }
-          if(food[i].category === '일식'){
-            japen.push(food[i].name)
-          }
+          allFood.push(food[i].name)
         }
-        console.log(korea);
-        console.log(china);
-        console.log(american);
-        console.log(japen);
+        console.log(allFood);
+        const food9 = []
+        for(let i = 0; food9.length < 9; i++){
+          let a = allFood[Math.floor(Math.random() * allFood.length)]
+          food9.includes(a) ? undefined : food9.push(a)
+        if(food9.length === 9){
+          product = [...food9]
+          break;
+        }
+      }
+      newMake();
       }
   });
 }
@@ -39,6 +32,7 @@ const foodKorea = () => {
         type: "GET",
         url: "/food_db",
         data: {},
+        async:false,
         success: function (response) {
           const food = response['foods']
           const korea = []
@@ -47,17 +41,110 @@ const foodKorea = () => {
               korea.push(food[i].name)
             }
           }
-          console.log(korea);
+          product.splice(0)
+          const korea9 = []
+          for(let i = 0; korea9.length < 9; i++){
+              let a = korea[Math.floor(Math.random() * korea.length)]
+              korea9.includes(a) ? undefined : korea9.push(a)
+            if(korea9.length === 9){
+              product = [...korea9]
+              break;
+            }
+          }
+          console.log(korea9);
+          newMake();
          }})
 }
 
 const foodChina = () => {
+  $.ajax({
+    type: "GET",
+    url: "/food_db",
+    data: {},
+    async:false,
+    success: function (response) {
+      const food = response['foods']
+      const china = []
+      for(let i = 0; i < food.length; i++){
+        if(food[i].category === '중식'){
+          china.push(food[i].name)
+        }
+      }
+      product.splice(0)
+      const china9 = []
+      for(let i = 0; china9.length < 9; i++){
+          let a = china[Math.floor(Math.random() * china.length)]
+          china9.includes(a) ? undefined : china9.push(a)
+        if(china9.length === 9){
+          product = [...china9]
+          break;
+        }
+      }
+      newMake();
+     }})
+}
 
+const foodJapen = () => {
+  $.ajax({
+    type: "GET",
+    url: "/food_db",
+    data: {},
+    async:false,
+    success: function (response) {
+      const food = response['foods']
+      const japen = []
+      for(let i = 0; i < food.length; i++){
+        if(food[i].category === '일식'){
+          japen.push(food[i].name)
+        }
+      }
+      product.splice(0)
+      const japen9 = []
+      for(let i = 0; japen9.length < 9; i++){
+          let a = japen[Math.floor(Math.random() * japen.length)]
+          japen9.includes(a) ? undefined : japen9.push(a)
+        if(japen9.length === 9){
+          product = [...japen9]
+          break;
+        }
+      }
+      console.log(japen9);
+      newMake();
+     }})
+}
+
+const foodAmerica = () => {
+  $.ajax({
+    type: "GET",
+    url: "/food_db",
+    data: {},
+    async:false,
+    success: function (response) {
+      const food = response['foods']
+      const america = []
+      for(let i = 0; i < food.length; i++){
+        if(food[i].category === '양식'){
+          america.push(food[i].name)
+        }
+      }
+      product.splice(0)
+      const america9 = []
+      for(let i = 0; i < america.length; i++){
+        if(america9.length < 9){
+          let a = america[Math.floor(Math.random() * america.length)]
+          america9.includes(a) ? null : america9.push(a)
+            }
+        if(america9.length === 9){
+          product = [...america9]
+          break;
+        }
+      }
+      newMake();
+     }})
 }
 
 
-
-const product = [
+let product = [
   "떡볶이", '돈가스', "초밥", "피자", "냉면", "치킨", '족발', "피자", "삼겹살"
 ];
 
