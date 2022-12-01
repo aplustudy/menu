92abdb7643477d5847111e4580a7f14672740640
qbg5652 <qbg5652@naver.com>
2022-12-01 18:33:50 +0900

    해시적용완료, db연결완료, 블루프린트 구분은 아직

---
+++ food_db.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+import certifi
+from pymongo import MongoClient
+
+food_db = Blueprint("food_db", __name__, template_folder="templates")
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:1zVXDk4pUjJSb8Qy@cluster0.htk9v1y.mongodb.net/test?retryWrites=true&w=majority', tlsCAFile = ca)
+db = client.dbsparta
+
+@food_db.route("/") 		
+def food_get():
+    food_list = list(db.food.find({}, {'_id': False}))
+    return jsonify({'foods' : food_list})
