4ec919fdbb2c7f59a50034c3deb79e803d582a40
ParkIlha <fafa0919999@gmail.com>
2022-12-05 23:37:43 +0900

    사다리타기 기능 완

--- static/js/ladder.js
+++ static/js/ladder.js
@@ -1,21 +1,42 @@
 const fooddata = []
 
 const randomValue = (array) => {
-    const random  = Math.floor(Math.random() * array.length)
-    return array[random];
+    if(array.length === 0){
+        const random  = Math.floor(Math.random() * array.length)
+        return array[random];
+    }else{
+        const random  = Math.floor(Math.random() * array.length)
+        return array[random];
+    }
 }
 
 
-let food = randomValue(fooddata);
 
-
-const add_textbox = () => {
+const addTextbox = () => {
     const box = document.getElementById("ladder");
-    const newP = document.createElement('p');
-    const foodvalue = document.getElementById('want_food').value
-    newP.innerHTML = "<p>"+foodvalue+"</p><input type='button' value='삭제' onclick='remove(this)'>";
-    box.appendChild(newP);
+    const newP = document.createElement('div');
+    newP.classList.add('p_tag')
+    const foodValue = document.getElementById('want_food').value
+    if(foodValue === ""){
+        alert("메뉴를 입력해주셔야죠..")
+    }
+    else {
+        newP.innerHTML = "<p class='ladder_p_tag'>" + foodValue + "</p><input type='button' class='ladder_btn' value='삭제' onclick='remove(this)'>";
+        box.appendChild(newP);
+        fooddata.push(foodValue);
+        parent.document.getElementById('want_food').value='';
+    }
 }
 const remove = (obj) => {
     document.getElementById('ladder').removeChild(obj.parentNode);
+}
+
+const randomFood = () => {
+    let food = randomValue(fooddata);
+    if(food === undefined) {
+        alert("음식 안드시려구요?")
+    }
+    else{
+        alert("오늘의 밥은 " + food + " 드셔야겠네요")
+    }
 }
