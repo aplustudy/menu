2f11013fa4147692e30c568439f05ed953ff43d2
qbg5652 <qbg5652@naver.com>
2022-12-06 20:25:30 +0900

    회원가입 블루프린트 적용완료

---
+++ join.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, jsonify, Blueprint
+from pymongo import MongoClient
+import certifi
+import jwt
+import datetime
+import hashlib
+
+join = Blueprint("join", __name__, template_folder="templates")
+
+ca = certifi.where()
+client = MongoClient("mongodb+srv://bongdroid:qhdrbs88!@cluster0.hecgbmx.mongodb.net/Cluster0?retryWrites=true&w=majority", tlsCAFile = ca)
+db = client.users
+
+SECRET_KEY = 'omemu4$'
+
+
+# @join.route('/')
+# def main():
+#     return render_template('join.html')
+
+@join.route('/')
+def joincall():
+    return render_template('join.html')
+
+# 회원가입
+@join.route('/', methods=['POST'])
+def api_register():
+    email_receive = request.form['email_give']
+    password_receive = request.form['password_give']
+    name_receive = request.form['name_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    # 이미 존재하는 아이디면 패스!
+    result = db.user.find_one({'email': email_receive})
+    if result is not None:
+        return jsonify({'result': 'fail', 'msg': '이미 존재하는 ID(이메일)입니다.'})
+    else:
+        db.user.insert_one({'email': email_receive, 'password': password_hash, 'name': name_receive})
+        return jsonify({'result': 'success'})
---
+++ login.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, jsonify, Blueprint
+from pymongo import MongoClient
+import certifi
+import jwt
+import datetime
+import hashlib
+
+login = Blueprint("login", __name__, template_folder="templates")
+
+ca = certifi.where()
+client = MongoClient("mongodb+srv://bongdroid:qhdrbs88!@cluster0.hecgbmx.mongodb.net/Cluster0?retryWrites=true&w=majority", tlsCAFile = ca)
+db = client.users
+
+SECRET_KEY = 'omemu4$'
+
+@login.route('/')
+def logincall():
+    return render_template('login.html')
+
+# 로그인
+@login.route('/', methods=['POST'])
+def api_login():
+    email_receive = request.form['email_give']
+    password_receive = request.form['password_give']
+
+    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
+    result = db.user.find_one({'email': email_receive, 'password': password_hash})
+
+    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    if result is not None:
+        # JWT 토큰 생성
+        payload = {
+            'email': email_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        # token을 줍니다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+# 보안: 로그인한 사용자만 통과할 수 있는 API
+@login.route('/isAuth', methods=['GET'])
+def api_valid():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        # token을 시크릿키로 디코딩합니다.
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
+        userinfo = db.user.find_one({'email': payload['email']}, {'_id': 0})
+        return jsonify({'result': 'success', 'name': userinfo['name']})
+    except jwt.ExpiredSignatureError:
+        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        # 로그인 정보가 없으면 에러가 납니다!
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
