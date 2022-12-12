47290a44ef6f7a987f68416d9a22d47fb895f978
oyeon04 <oyeon04@gmail.com>
2022-12-12 09:33:55 +0900

    게시판 연동

--- views/forms.py
+++ views/forms.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired
 class BoardForm(FlaskForm):
     title = StringField('제목', validators=[DataRequired('제목을 입력해주세요!')])
     content = TextAreaField('내용', validators=[DataRequired('내용을 입력해주세요!')])
+    login_name = TextAreaField('user')
     #StringField 글자수 제한, TextAreaField 글자수 제한 없음
 
 class CommentForm(FlaskForm):
