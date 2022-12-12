7c484f799576d9b41d4e2553db66e9c66e9a9830
oyeon04 <oyeon04@gmail.com>
2022-12-12 21:54:43 +0900

    내가 작성한 글 조회 추가, 사진 첨부 50%

--- views/user_page.py
+++ views/user_page.py
@@ -1,7 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+from pymongo import MongoClient
+import certifi
+import jwt
 
 bp = Blueprint("user_page", __name__, template_folder="templates")
 
+ca = certifi.where()
+client = MongoClient("mongodb+srv://bongdroid:qhdrbs88!@cluster0.hecgbmx.mongodb.net/Cluster0?retryWrites=true&w=majority", tlsCAFile = ca)
+db = client.users
+SECRET_KEY = 'omemu4$'
+
 @bp.route("/") 		
 def main():
-    return render_template("user_page.html")
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        userinfo = db.user.find_one({'email': payload['email']}, {'_id': 0})
+        user_email = userinfo['email']
+    except:
+        user_email = userinfo['익명']
+    return render_template("user_page.html", user_email = user_email)
